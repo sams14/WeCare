9db5c9a19a4ffcc697a302920f1ecbac3b082864
Chandan <sahooc029@gmail.com>
2021-07-15 12:49:23 +0530

    🔥

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,31 +1,18 @@
 import React from "react";
-import { Switch, Route,Redirect } from "react-router";
+import { Switch, Route, Redirect } from "react-router";
 import Home from './pages/Home';
 
 const MainComponent = () => {
-<<<<<<< HEAD
-  return (
-    <div>
-      <Switch>
-        <Route path="/home" exact>
-          <Home />
-        </Route>
-        <Redirect from="*" to="/home" />
-      </Switch>
-    </div>
-  );
-=======
     return (
         <div>
             <Switch>
                 <Route path="/home" exact>
                     <Home />
                 </Route>
-
+                <Redirect from="*" to="/home" />
             </Switch>
         </div>
     );
->>>>>>> 67a300c03635dab816c5212d423bed9880c03c4a
 };
 
 export default MainComponent;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
 import HeroSection from '../components/HeroSection/Hero';
-=======
-import FirstLandingPage from './FirstLandingPage'
->>>>>>> 67a300c03635dab816c5212d423bed9880c03c4a
 
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
-            <HeroSection/>
-=======
-            <FirstLandingPage />
->>>>>>> 67a300c03635dab816c5212d423bed9880c03c4a
+            <HeroSection />
         </div>
     )
 }
