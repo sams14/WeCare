25fe67525f8ce5bea72fc3f911cf5c7fdf5371b2
nmbaibhab <nmbaibhab@gmail.com>
2021-07-16 11:02:03 +0530

    home page updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import HeroDescription from '../components/HeroSection/HeroDescription';
 import Hero from '../components/HeroSection/Hero';
-<<<<<<< HEAD
 import  HeroAppDownload  from '../components/HeroSection/HeroAppDownload';
 import Statistic from '../components/Statistics/Statistic';
 import Footer from '../components/Footer/Footer';
-=======
 import Features from '../components/Features/Features';
->>>>>>> e81d7748a011ee46ffec4705588f57d1da1094b4
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <HeroDescription />
-<<<<<<< HEAD
             <HeroAppDownload/>
             <Statistic/>
             <Footer/>
-=======
             <Features />
->>>>>>> e81d7748a011ee46ffec4705588f57d1da1094b4
         </div>
     )
 }
