a4a10f920fa1e0d235abb183d50d49468442edbc
nmbaibhab <nmbaibhab@gmail.com>
2021-07-16 11:00:06 +0530

    ✨ new features added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import HeroDescription from '../components/HeroSection/HeroDescription';
 import Hero from '../components/HeroSection/Hero';
+import  HeroAppDownload  from '../components/HeroSection/HeroAppDownload';
+import Statistic from '../components/Statistics/Statistic';
+import Footer from '../components/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <HeroDescription />
+            <HeroAppDownload/>
+            <Statistic/>
+            <Footer/>
         </div>
     )
 }
